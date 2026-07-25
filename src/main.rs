6e5a327572b7169307c6fd1c10b9bcea5401908a
疒奀 <yz@mmon.co>
2026-07-25 08:29:14 -0700

    feat: bgm with layers active based on skills

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ pub mod on_turn_timer;
 pub mod altar_ui;
 pub mod altar;
 pub mod conveyor_belt;
+pub mod music;
 
 use std::collections::HashSet;
 use crate::ecs::{CompletedTurn, DebugMode, ObstructedSet, SignalLayers, SignalSystems, TurnCounter};
@@ -27,6 +28,7 @@ use crate::global_periodic_timer::global_periodic_timer_plugin;
 use crate::on_turn_timer::on_turn_timer_plugin;
 use crate::pressure_plate::pressure_plate_plugin;
 use crate::signal_extension_timer::signal_extension_timer_plugin;
+use crate::music::music_plugin;
 
 pub const MAX_TURN_COUNT: u32 = 1000;
 
@@ -62,6 +64,7 @@ fn main() {
         .add_plugins(activated_periodic_timer_plugin)
         .add_plugins(signal_extension_timer_plugin)
         .add_plugins(on_turn_timer_plugin)
+        .add_plugins(music_plugin)
         .add_systems(Update, clear_signal_layers.in_set(SignalSystems::Clear).after(movement::do_movement))
         .configure_sets(Update, (SignalSystems::Clear, SignalSystems::Write, SignalSystems::Timer, SignalSystems::Read).chain())
         .run();
